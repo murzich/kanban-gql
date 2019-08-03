62f3ca42194831c5b15ad4fc08ef97b700049d88
murzich <destabilitron@gmail.com>
2019-08-03 14:49:15 +0300

    IssueCard - render RED #1

---
+++ src/Issue/__tests__/IssueCard.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import IssueCard from "../IssueCard";
+
+describe("IssueCard", () => {
+  it("should be rendered", () => {
+    const { getByTestId } = render(<IssueCard />);
+
+    expect(getByTestId("issueCard")).toBeDefined();
+  });
+});
