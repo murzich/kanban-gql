391cf0a72490b488cd4e64d056cc1b2318115bda
murzich <destabilitron@gmail.com>
2019-08-04 10:21:06 +0300

    Applied Blueprint styles & removed template text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
+import { Navbar } from '@blueprintjs/core';
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
+import styles from './App.module.scss';
+import IssueCard from './Issue/IssueCard';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.root}>
+      <Navbar>
+        <Navbar.Group>
+          Kanban GraphQL TDD
+        </Navbar.Group>
+      </Navbar>
+      <main className={styles.content}>
+        <IssueCard />
+      </main>
     </div>
   );
 };
--- src/Issue/IssueCard.tsx
+++ src/Issue/IssueCard.tsx
@@ -1,8 +1,11 @@
-import { Card } from '@blueprintjs/core';
+import { Card, Elevation } from '@blueprintjs/core';
 import React from 'react';
 
-const IssueCard: React.FC = () => {
-  return <Card data-testid="issueCard" />;
-};
+const IssueCard: React.FC = () => (
+  <Card data-testid="issueCard" interactive elevation={Elevation.TWO}>
+    {/* TODO: remove temp text */}
+    <div>Temp text</div>
+  </Card>
+);
 
 export default IssueCard;
