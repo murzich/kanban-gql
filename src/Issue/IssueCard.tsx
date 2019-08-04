eb7ba0e8391713c4983f38e8ad727aafe7c04406
murzich <destabilitron@gmail.com>
2019-08-04 12:14:08 +0300

    IssueCard - issue summary: GREEN

--- src/Issue/IssueCard.tsx
+++ src/Issue/IssueCard.tsx
@@ -4,10 +4,13 @@ import React from 'react';
 type IssueProps = {
   // Issue unique id like "TEST-123"
   id?: string; // TODO: change to required?
+  // Issue summary or title
+  summary?: string;
 };
 
-const IssueCard: React.FC<IssueProps> = ({ id }) => (
+const IssueCard: React.FC<IssueProps> = ({ id, summary }) => (
   <Card role="article" interactive elevation={Elevation.TWO}>
+    <h2>{summary}</h2>
     <h3>{id}</h3>
     {/* TODO: remove temp text */}
     <div>Temp text</div>
