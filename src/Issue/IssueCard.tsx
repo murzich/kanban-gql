ddd0f07d3b8775f9d95364a8c584bd0a82e18314
murzich <destabilitron@gmail.com>
2019-08-04 11:33:04 +0300

    IssueCard - issue id: GREEN

--- src/Issue/IssueCard.tsx
+++ src/Issue/IssueCard.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 const IssueCard: React.FC = () => (
   <Card role="article" interactive elevation={Elevation.TWO}>
+    <h3>TEST-123</h3>
     {/* TODO: remove temp text */}
     <div>Temp text</div>
   </Card>
