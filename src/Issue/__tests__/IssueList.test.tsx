947f9013d0b7242f0edea97cd125ad7fbd51c0e7
murzich <destabilitron@gmail.com>
2019-08-04 22:28:59 +0300

    IssueList - items: RED❌

--- src/Issue/__tests__/IssueList.test.tsx
+++ src/Issue/__tests__/IssueList.test.tsx
@@ -5,9 +5,42 @@ import IssueList from '../IssueList';
 afterEach(cleanup);
 
 describe('IssueList', () => {
-  it('be rendered', () => {
+  it('should be rendered', () => {
     const { getByRole } = render(<IssueList />);
 
     expect(getByRole('list')).toBeDefined();
+  });
+
+  it('displays passed Issue items data', () => {
+    const issues = [
+      {
+        id: 'TEST-1',
+        summary: 'Test summary 1',
+        description: 'Test issue description 1',
+      },
+      {
+        id: 'TEST-2',
+        summary: 'Test summary 2',
+        description: 'Test issue description 2',
+      },
+      {
+        id: 'TEST-3',
+        summary: 'Test summary 3',
+        description: 'Test issue description 3',
+      },
+      {
+        id: 'TEST-4',
+        summary: 'Test summary 4',
+        description: 'Test issue description 4',
+      },
+    ];
+
+    const { getByText } = render(<IssueList issues={issues} />);
+
+    issues.forEach((issue) => {
+      Object.values(issue).map((text) => {
+        expect(getByText(text)).toBeDefined();
+      })
+    })
   });
 });
