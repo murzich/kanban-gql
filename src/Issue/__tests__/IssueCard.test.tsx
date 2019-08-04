25cf92339e542771cdea666ed0572cd2c9f93a8d
murzich <destabilitron@gmail.com>
2019-08-04 11:08:28 +0300

    IssueCard - issue id: RED

--- src/Issue/__tests__/IssueCard.test.tsx
+++ src/Issue/__tests__/IssueCard.test.tsx
@@ -1,6 +1,8 @@
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import React from 'react';
 import IssueCard from '../IssueCard';
+
+afterEach(cleanup);
 
 describe('IssueCard', () => {
   it('should be rendered', () => {
@@ -8,4 +10,10 @@ describe('IssueCard', () => {
 
     expect(getByRole('article')).toBeDefined();
   });
+
+  it('exposes an Issue ID', () => {
+    const { getByText} = render(<IssueCard />);
+
+    expect(getByText('TEST-123')).toBeDefined;
+  })
 });
