df85fd57be7748c97efc86b32d12daaa70d0e6b8
murzich <destabilitron@gmail.com>
2019-08-08 00:41:28 +0300

    Loading - spinner: GREEN✔

--- src/shared/Loading.tsx
+++ src/shared/Loading.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import { Spinner } from '@blueprintjs/core';
+import { Spinner, ISpinnerProps } from '@blueprintjs/core';
 
-const Loading: React.FC = () => <Spinner />;
+type LoadingType = ISpinnerProps & React.HTMLProps<HTMLDivElement>;
+
+const Loading: React.FC<LoadingType> = ({ ref, ...props }) => (
+  <div
+    ref={ref}
+    aria-busy="true"
+    aria-live="polite"
+    aria-label="Loading…"
+    role="figure"
+  >
+    <Spinner {...props} />
+  </div>
+);
 
 export default Loading;
--- src/shared/__tests__/Loading.tsx
+++ src/shared/__tests__/Loading.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
+import React from 'react';
 import Loading from '../Loading';
 
 describe('Loading', () => {
   it('should be rendered', () => {
-    const { getByLabelText } = render(<Loading />);
+    const { getByRole } = render(<Loading />);
 
-    expect(getByLabelText('Loading…')).toBeDefined();
+    expect(getByRole('figure')).toHaveAttribute('aria-label', 'Loading…');
   });
 });
