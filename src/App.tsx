6fda5219fa90b61004994f58679101d605acf190
Ihor Murzich <murzichigor@outlook.com>
2019-08-08 15:27:35 +0300

    IssueListContainer - fetch issues: REFACTOR👨‍🔧

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
+import { ApolloProvider } from '@apollo/react-hooks';
 import { Navbar } from '@blueprintjs/core';
+import ApolloClient from 'apollo-boost';
 import React from 'react';
 import styles from './App.module.scss';
 import IssueCard from './Issue/IssueCard';
 import IssueList from './Issue/IssueList';
+import IssueListContainer from './Issue/IssueListContainer';
+
+const client = new ApolloClient({
+  uri: 'https://faker.graphqleditor.com/kanban/kanban/graphql',
+});
 
 const App: React.FC = () => {
   return (
-    <div className={styles.root}>
-      <Navbar>
-        <Navbar.Group>Kanban GraphQL TDD</Navbar.Group>
-      </Navbar>
-      <main className={styles.content}>
-        <IssueCard
-          id="TEMP-123"
-          summary="Temp summary"
-          description="Temporary description text"
-        />
-        <IssueList />
-      </main>
-    </div>
+    <ApolloProvider client={client}>
+      <div className={styles.root}>
+        <Navbar>
+          <Navbar.Group>Kanban GraphQL TDD</Navbar.Group>
+        </Navbar>
+        <main className={styles.content}>
+          <IssueCard
+            id="TEMP-123"
+            summary="Temp summary"
+            description="Temporary description text"
+          />
+          <IssueList />
+          <IssueListContainer />
+        </main>
+      </div>
+    </ApolloProvider>
   );
 };
 
