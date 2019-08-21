b533d9fe3e5ae830382f10a99fc79e0f4f361328
murzich <destabilitron@gmail.com>
2019-08-21 21:57:45 +0300

    ColumnItem - display Issues: RED❌

--- src/Columns/__tests__/ColumnItem.test.tsx
+++ src/Columns/__tests__/ColumnItem.test.tsx
@@ -1,13 +1,28 @@
-import { render } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import React from 'react';
+import IssueList from '../../Issue/IssueList';
 import ColumnItem from '../ColumnItem';
 
 describe('ColumnItem', () => {
+  afterEach(cleanup);
   test('should has title', () => {
     const stubTitle = 'Column title';
 
     const { getByText } = render(<ColumnItem title={stubTitle} />);
 
     expect(getByText(stubTitle)).toBeDefined();
+  });
+
+  test('should display passed Issue cards', () => {
+    const mockedIssues = [
+      { id: 1, summary: 'test summary', description: 'test description' },
+    ];
+    const { getByRole } = render(
+      <ColumnItem title="Test column">
+        <IssueList issues={mockedIssues} />
+      </ColumnItem>,
+    );
+
+    expect(getByRole('article')).toBeDefined();
   });
 });
