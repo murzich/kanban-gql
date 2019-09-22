60d65a84eb2d719960c11fd91157838d87d1a9a8
murzich <destabilitron@gmail.com>
2019-09-23 02:24:43 +0300

    NewIssueForm - Added submitting form😏

--- src/Forms/NewIssueForm.tsx
+++ src/Forms/NewIssueForm.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import { Form } from 'react-final-form';
+import { Config } from 'final-form';
+import { Form, Field } from 'react-final-form';
 import { FormGroup, InputGroup, TextArea, Button } from '@blueprintjs/core';
 
-const NewIssueForm = () => {
+type IssueValues = {
+  summary: string;
+  description?: string;
+};
+
+type Props = {
+  onSubmit: (values?: IssueValues) => void;
+};
+
+const NewIssueForm: React.FC<Props> = ({ onSubmit }) => {
   return (
-    <Form onSubmit={() => {}}>
+    <Form onSubmit={onSubmit}>
       {({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
           <FormGroup label="Summary" labelFor="summary" labelInfo="(required)">
-            <InputGroup
-              id="summary"
-              placeholder="Type summary for the issue"
-              required
-            />
+            <Field name="summary">
+              {({ input = {} }) => (
+                <InputGroup
+                  id="summary"
+                  placeholder="Type summary for the issue"
+                  required
+                  {...input}
+                />
+              )}
+            </Field>
           </FormGroup>
 
           <FormGroup label="Description" labelFor="description">
-            <TextArea
-              id="description"
-              placeholder="Type description for the issue"
-              fill
-              growVertically
-            />
+            <Field name="description">
+              {({ input = {} }) => (
+                <TextArea
+                  id="description"
+                  placeholder="Type description for the issue"
+                  fill
+                  growVertically
+                  {...input}
+                />
+              )}
+            </Field>
           </FormGroup>
 
           <Button type="submit" icon="saved">
--- src/Forms/__tests__/NewIssueForm.test.tsx
+++ src/Forms/__tests__/NewIssueForm.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import NewIssueForm from '../NewIssueForm';
 
@@ -10,7 +10,7 @@ describe('New Issue Form', () => {
     const inputNames = [/summary/i, /description/i];
     expect.assertions(inputNames.length);
 
-    const { getByLabelText } = render(<NewIssueForm />);
+    const { getByLabelText } = render(<NewIssueForm onSubmit={() => {}} />);
 
     inputNames.forEach(id =>
       expect(getByLabelText(id, { selector: 'input, textarea' })).toBeDefined(),
@@ -18,12 +18,47 @@ describe('New Issue Form', () => {
   });
 
   test('should contain submit button', () => {
-    const { getByText } = render(<NewIssueForm />);
+    const { getByText } = render(<NewIssueForm onSubmit={() => {}} />);
 
     // .closest added to select button Element even if it contain another elements inside
     expect(getByText(/create/i).closest('button')).toHaveAttribute(
       'type',
       'submit',
+    );
+  });
+
+  test('should call onSubmit callback on button click', () => {
+    const onSubmitMock = jest.fn();
+    const { getByText } = render(<NewIssueForm onSubmit={onSubmitMock} />);
+
+    fireEvent.click(getByText(/create/i).closest('button') || document);
+
+    expect(onSubmitMock).toHaveBeenCalledTimes(1);
+  });
+
+  test('should submit typed values from inputs', () => {
+    const onSubmitMock = jest.fn();
+    const stubValues = [
+      { label: /summary/i, value: 'testSummary', name: 'summary' },
+      {
+        label: /description/i,
+        value: 'test description\nnext line',
+        name: 'description',
+      },
+    ];
+
+    const { getByText, getByLabelText } = render(
+      <NewIssueForm onSubmit={onSubmitMock} />,
+    );
+
+    stubValues.forEach(({ label, value }) => {
+      fireEvent.change(getByLabelText(label), { target: { value } });
+    });
+
+    fireEvent.click(getByText(/create/i).closest('button') || document);
+
+    expect(onSubmitMock.mock.calls[0][0]).toEqual(
+      stubValues.reduce((a, { name, value }) => ({ ...a, [name]: value }), {}),
     );
   });
 });
